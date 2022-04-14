c695ecf1baf654dbf9d2ac2e2091c345690393dd
JongOk Lee <>
2022-04-14 13:39:07 +0900

    bundle setting ui toast message

--- com.crazybox.baseframework/Runtime/UILibs/Scripts/Common Frames/cxUIToastMessageFrame.cs
+++ com.crazybox.baseframework/Runtime/UILibs/Scripts/Common Frames/cxUIToastMessageFrame.cs
@@ -6,14 +6,27 @@ using UnityEngine.UI;
 
 public class cxUIToastMessageFrame : cxUIFrame
 {
+    public enum MsgType {
+        Normal,
+        Warning
+    }
+
     [SerializeField]
     private cxUITweenPanel panel;
+    [SerializeField]
+    private Image background;
+
     [SerializeField]
     private float hideTime = 3.0f;
 
     [SerializeField]
     private Text message;
 
+    [SerializeField]
+    private Color normalMessageBg = Color.blue;
+    [SerializeField]
+    private Color warningMessageBg = Color.yellow;
+    
     protected override void OnInit()
     {
         //cxUINavigator.Instance.PushFrame<cxUIToastMessageFrame>();
@@ -29,8 +42,9 @@ public class cxUIToastMessageFrame : cxUIFrame
         
     }
 
-    public void Toast(string message ){
+    public void Toast(string message, MsgType msgType = MsgType.Normal ){
         this.message.text = message;
+        this.background.color = msgType == MsgType.Normal ? normalMessageBg : warningMessageBg;
         panel.Play();
         panel.ScheduledHide(hideTime);
     }
