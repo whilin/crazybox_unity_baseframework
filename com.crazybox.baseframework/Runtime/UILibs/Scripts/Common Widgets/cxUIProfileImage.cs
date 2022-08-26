473b0f475338a62df15b1e879c792a61f57b561b
JongOk Lee <>
2022-08-26 16:13:08 +0900

    Added, ProfileImage Cache

--- com.crazybox.baseframework/Runtime/UILibs/Scripts/Common Widgets/cxUIProfileImage.cs
+++ com.crazybox.baseframework/Runtime/UILibs/Scripts/Common Widgets/cxUIProfileImage.cs
@@ -1,57 +1,71 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.UI;
 using UniRx;
 using UniRx.Triggers;
+using UnityEngine;
 using UnityEngine.Networking;
+using UnityEngine.UI;
 
-[RequireComponent(typeof(Mask))]
-public class cxUIProfileImage : MonoBehaviour
-{
+[RequireComponent (typeof (Mask))]
+public class cxUIProfileImage : MonoBehaviour {
+    static Dictionary<string, Texture> Caching = new Dictionary<string, Texture> ();
+
     [SerializeField]
     private RawImage photoImage;
     [SerializeField]
     private string url;
 
-    void Awake()
-    {
+    public static void ClearCaching() {
+        Caching.Clear();
+    }
+
+    void Awake () {
 
     }
-    
-    private void Start()
-    {
-        if (!string.IsNullOrEmpty(url))
-            Load(url);
+
+    private void Start () {
+        if (!string.IsNullOrEmpty (url))
+            Load (url);
         else
-            SetEmpty();
+            SetEmpty ();
     }
 
-    public void Load(string url)
-    {
+    public void Load (string url) {
         this.url = url;
-        SetEmpty();
+        SetEmpty ();
 
-        if (!string.IsNullOrEmpty(url))
-            UnityWebRequestTexture.GetTexture(url)
-                .SendWebRequest()
-                .AsObservable()
-                .Subscribe(op =>
-                {
-                    UnityWebRequestAsyncOperation op2 = (UnityWebRequestAsyncOperation)op;
+        if (!string.IsNullOrEmpty (url)) {
 
-                    if (op2.isDone && op2.webRequest.result == UnityWebRequest.Result.Success)
-                    {
-                        Texture myTexture = ((DownloadHandlerTexture)op2.webRequest.downloadHandler).texture;
-                        photoImage.color = Color.white;
-                        photoImage.texture = myTexture;
-                    }
+            if (Caching.TryGetValue (url, out Texture cachedTexture)) {
+                photoImage.color = Color.white;
+                photoImage.texture = cachedTexture;
 
-                }).AddTo(this);
+            } else {
+
+                UnityWebRequestTexture.GetTexture (url)
+                    .SendWebRequest ()
+                    .AsObservable ()
+                    .Subscribe (op => {
+                        UnityWebRequestAsyncOperation op2 = (UnityWebRequestAsyncOperation) op;
+
+                        if (op2.isDone && op2.webRequest.result == UnityWebRequest.Result.Success) {
+                            Texture myTexture = ((DownloadHandlerTexture) op2.webRequest.downloadHandler).texture;
+                            photoImage.color = Color.white;
+                            photoImage.texture = myTexture;
+
+                           SaveCache (url, myTexture);
+                        }
+
+                    }).AddTo (this);
+            }
+        }
     }
 
-    void SetEmpty()
-    {
+    void SaveCache(string url, Texture myTexture){
+         Caching.Add (url, myTexture);
+    }
+
+    void SetEmpty () {
         photoImage.color = Color.gray;
         photoImage.texture = null;
     }
