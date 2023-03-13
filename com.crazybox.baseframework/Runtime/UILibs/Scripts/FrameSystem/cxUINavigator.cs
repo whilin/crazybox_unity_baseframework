04342aa2bdbc6a5212ab38b3b864f8e012f39a4e
JongOk Lee <>
2023-03-13 17:53:43 +0900

    Added, UINavigator.PopAllFrames

--- com.crazybox.baseframework/Runtime/UILibs/Scripts/FrameSystem/cxUINavigator.cs
+++ com.crazybox.baseframework/Runtime/UILibs/Scripts/FrameSystem/cxUINavigator.cs
@@ -23,15 +23,16 @@ public class cxUINavigator : MonoSingleton<cxUINavigator> {
         }
     }
 
-    public T FindFrame<T>() where  T : cxUIFrame
-    {
-        return cxUIFrame.FindFrame<T>();
+    public T FindFrame<T> () where T : cxUIFrame {
+        return cxUIFrame.FindFrame<T> ();
     }
 
     public cxUIFrame ReplacePushFrame<T> (object showParam = null) where T : cxUIFrame {
         var frame = frames.Find (q => q.GetType () == typeof (T));
-        if (frame == null || frame.IsDialog)
-            throw new Exception("PushFrame FrameType is Not Frame");
+        if (frame == null)
+            throw new Exception ("ReplacePushFrame FrameType not found type:" + typeof (T));
+        if (frame.IsDialog)
+            throw new Exception ("ReplacePushFrame FrameType is Not Frame type:" + typeof (T));
 
         cxUIContext context = new cxUIContext (showParam);
         frame.Replace (context, showParam);
@@ -41,8 +42,10 @@ public class cxUINavigator : MonoSingleton<cxUINavigator> {
     public cxUIFrame PushFrame<T> (object showParam = null) where T : cxUIFrame {
         var frame = frames.Find (q => q.GetType () == typeof (T));
 
-        if (frame == null || frame.IsDialog)
-            throw new Exception("PushFrame FrameType is Not Frame");
+        if (frame == null)
+            throw new Exception ("PushFrame FrameType not found type:" + typeof (T));
+        if (frame.IsDialog)
+            throw new Exception ("PushFrame FrameType is Not Frame type:" + typeof (T));
 
         cxUIContext context = new cxUIContext (showParam);
         frame.Push (context, showParam);
@@ -55,14 +58,22 @@ public class cxUINavigator : MonoSingleton<cxUINavigator> {
             cxUIFrame.ActiveFrame.Pop ();
     }
 
+    public void PopAllFrames(){
+        while(cxUIFrame.ActiveFrame){
+            cxUIFrame.ActiveFrame.Pop();
+        }
+    }
+
     //Note.
     //  1. 팝업은 단독으로 나타날 수 없으며, 프레임위에서만 보여진다.
     //  2. 팝업의 라이프사이클은 베이스프레임에 종속되어 있다.
     public cxUIFrame ShowDialog<T> (object showParam = null) where T : cxUIFrame {
         var frame = frames.Find (q => q.GetType () == typeof (T));
 
-        if (frame == null || !frame.IsDialog)
-            throw new Exception("ShowDialog FrameType is Not Dialog");
+        if (frame == null)
+            throw new Exception ("PushFrame FrameType not found type:" + typeof (T));
+        if (!frame.IsDialog)
+            throw new Exception ("PushFrame FrameType is Not Dialog type:" + typeof (T));
 
         cxUIContext context = new cxUIContext (showParam);
         frame.PushPopup (context, showParam);
