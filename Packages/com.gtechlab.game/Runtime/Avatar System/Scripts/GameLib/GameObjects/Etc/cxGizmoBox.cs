e31565e37472e0c51ce55d9cbe706f07b009d6d8
JongOk Lee <>
2023-11-15 20:47:13 +0900

    v2.0.46 endo

--- Packages/com.gtechlab.game/Runtime/Avatar System/Scripts/GameLib/GameObjects/Etc/cxGizmoBox.cs
+++ Packages/com.gtechlab.game/Runtime/Avatar System/Scripts/GameLib/GameObjects/Etc/cxGizmoBox.cs
@@ -5,21 +5,35 @@ using UnityEngine;
 public class cxGizmoBox : MonoBehaviour
 {
 
-    public float size = 0.1f;
-    public Color color = Color.blue;
+   public enum GizmoType {
+      Sphere,
+      Axis
+   }
 
-    /// <summary>
-    /// Start is called on the frame when a script is enabled just before
-    /// any of the Update methods is called the first time.
-    /// </summary>
-    void Start()
-    {
-        
-    }
-   private void OnDrawGizmos() {
-    if(enabled) {
-       Gizmos.color = color;
-       Gizmos.DrawSphere(transform.position, size);
-    }
+   public GizmoType gizmoType = GizmoType.Sphere;
+   public float size = 0.1f;
+   public Color color = Color.blue;
+
+   /// <summary>
+   /// Start is called on the frame when a script is enabled just before
+   /// any of the Update methods is called the first time.
+   /// </summary>
+   void Start () {
+
+   }
+   private void OnDrawGizmos () {
+      if (enabled) {
+         if(gizmoType == GizmoType.Sphere) {
+            Gizmos.color = color;
+            Gizmos.DrawSphere (transform.position, size);
+         } else if(gizmoType == GizmoType.Axis) {
+            Gizmos.color = Color.blue;
+            Gizmos.DrawLine(transform.position, transform.position + transform.forward * size);
+            Gizmos.color = Color.green;
+            Gizmos.DrawLine(transform.position, transform.position + transform.up * size);
+            Gizmos.color = Color.red;
+            Gizmos.DrawLine(transform.position, transform.position + transform.right * size);
+         }
+      }
    }
 }
